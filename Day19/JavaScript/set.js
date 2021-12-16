2dd7e79573e4cbec08ccd9a43c6656e242095c45
ThanuPriyaL <65939146+ThanuPriyaL@users.noreply.github.com>
2021-12-16 16:21:08 +0530

    Add files via upload

---
+++ Day19/JavaScript/set.js
@@ -0,0 +1,10 @@
+class abc{
+    constructor(){
+        // setTimeout(function name(params){
+            setTimeout( () =>{
+            console.log('Hello this is SetTimeout function');
+        },5000)
+    }
+}
+
+var a=new abc();
